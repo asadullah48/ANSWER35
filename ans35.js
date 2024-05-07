54edcee30fa16bda6ec13c08da8e4da7aaf3a7ae
ASADULLAH SHAFIQUE <asadullahshafique@hotmail.com>
2024-05-07 13:17:41 +0500

    Initial commit

---
+++ ans35.js
@@ -0,0 +1,5 @@
+var animals = ["goat", "sheep", "camel", "cow"];
+animals.forEach(function (animals) {
+    console.log("animals ".concat(animals, " would be good for qurbani."));
+});
+console.log("Any of these animals would be good for qurbani, but my prefrence would be goat for this year, as i wish to purchase it bit earlier & take very special care of it, IN SHA ALLAH");
